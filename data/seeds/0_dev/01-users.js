1d2d0ab5c28f7eb51ccff6ea61355325f52f0472
Paul M Edwards <pauledwards@gmail.com>
2020-02-20 02:47:59 -0800

    Add more development seed data.

--- data/seeds/0_dev/01-users.js
+++ data/seeds/0_dev/01-users.js
@@ -4,10 +4,31 @@ exports.seed = function(knex) {
   return knex('users').insert([
     {
       //id: 1,
-      username: 'Test',
-      password: bcrypt.hashSync('Test', 10),
-      email: 'test@example.com',
+      username: 'TestAdmin',
+      password: bcrypt.hashSync('AdminPassword', 10),
+      email: 'testadmin@example.com',
       enabled: 1,
+    },
+    {
+      //id: 2,
+      username: 'TestUser1',
+      password: bcrypt.hashSync('User1Password', 10),
+      email: 'testuser1@example.com',
+      enabled: 1,
+    },
+    {
+      //id: 3,
+      username: 'TestUser2',
+      password: bcrypt.hashSync('User2Password', 10),
+      email: 'testuser2@example.com',
+      enabled: 1,
+    },
+    {
+      //id: 4,
+      username: 'TestUser3',
+      password: bcrypt.hashSync('User3Password', 10),
+      email: 'testuser3@example.com',
+      enabled: 0,
     },
   ]);
 };
--- data/seeds/0_dev/03-user_roles.js
+++ data/seeds/0_dev/03-user_roles.js
@@ -1,7 +1,13 @@
 exports.seed = function(knex) {
   return knex('user_roles').insert([
-    //Test
+    //TestAdmin
     { user_id: 1, role_id: 1 },   //Administrator
     { user_id: 1, role_id: 2 },   //User
+    //TestUser1
+    { user_id: 2, role_id: 2 },   //User
+    //TestUser2
+    { user_id: 3, role_id: 2 },   //User
+    //TestUser3
+    { user_id: 4, role_id: 2 },   //User
   ]);
 };
